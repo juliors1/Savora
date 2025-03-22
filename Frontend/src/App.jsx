deda67a43bafc307559221fd4c4eb29eff14ec2e
Julio Rojas <juliorojassolis@gmail.com>
2025-03-21 23:28:43 -0400

    fix main branch

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React from 'react'
 
 const App = () => {
-  return <div className="bg-red-400 text-white">App</div>;
-};
+  return (
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
