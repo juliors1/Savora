138e4e2036d35161cbc8729059c0085ceae5b1a8
Julio Rojas <juliorojassolis@gmail.com>
2025-04-01 00:32:56 -0400

    Finish Full CRUD for POST in controller

--- Backend/models/Post.js
+++ Backend/models/Post.js
@@ -4,16 +4,17 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema(
   {
     title: { type: String, required: true },
-    content: { type: String, required: true },
-    author: {
+    ingredients: { type: [String], required: true },
+    instructions: { type: String, required: true },
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
+    ratings: [{ userId: mongoose.Schema.Types.ObjectId, rating: Number }],
+    averageRating: { type: Number, default: 0 },
     createdAt: { type: Date, default: Date.now },
-    likes: { type: Number, default: 0 },
-    comments: { type: Number, default: 0 },
-    isPublic: { type: Boolean, default: true },
+    isPublic: { type: Boolean, default: true }, // To show in Explore Page (Recipe Board)
     image: {
       type: String,
       validate: {
@@ -33,6 +34,16 @@ const PostSchema = new Schema(
     timestamps: true,
   }
 );
+
+// Method to calculate average rating
+PostSchema.methods.calculateAverageRating = function () {
+  if (this.ratings.length === 0) {
+    this.averageRating = 0;
+  } else {
+    this.averageRating =
+      this.ratings.reduce((sum, r) => sum + r.rating, 0) / this.ratings.length;
+  }
+};
 
 const Post = mongoose.model("Post", postSchema);
 
